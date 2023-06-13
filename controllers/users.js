022949231f99dc9c0c46621cde100b4915527004
kaisadia <kaisa.diakhate@hotmail.com>
2023-06-13 09:29:08 +0300

    added requirements to user schema

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,7 @@ const User = require('../models/users')
 const bcrypt = require('bcrypt')
 
 usersRouter.get('/', async (request, response) => {
-    const users = await User.find({})
+    const users = await User.find({}).populate('blogs')
     response.json(users)
 })
 
